abed0878cae4b9b200d77d0ad354eb91b7360eb5
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-02 14:32:50 +0530

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,19 @@
+public class VariableDemo{
+    public static void main(String[] args){
+        String name = "Akanksha";
+        int age = 20;
+        String college = "NMIET";
+        String branch = "Computer Engineering";
+        double percentage = 90.25;
+        char grade = 'A';
+        boolean ispassed = true;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("College: " + college);
+        System.out.println("Branch: " + branch);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Passed: " + ispassed);
+
+    }
+    }
