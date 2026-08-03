0e33400368c27b1fdbe370ee09f00ad0010e6953
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-03 17:05:56 +0530

    Add files via upload

---
+++ Arithmetic3.java
@@ -0,0 +1,14 @@
+public class Arithmetic3{
+    public static void main(String[] args){
+        int num1 = 50;
+        int num2 = 15;
+        System.out.println("Number1:" + num1);
+        System.out.println("Number2:" + num2);
+        System.out.println("Addition:" + (num1 + num2));
+        System.out.println("Substraction:" + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Modulo:" + (num1 % num2 ));
+        
+    }
+}
