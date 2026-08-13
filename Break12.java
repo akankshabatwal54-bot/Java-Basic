4dec6caca2bf63cfceab830a9ee5c62f070bd967
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-08-13 21:58:14 +0530

    Add files via upload

---
+++ Break12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Break12{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        for(int i = 1; i <= 5; i++){
+            System.out.println("Enter a number: ");
+            int num = sc.nextInt();
+
+            if(num == 0){
+
+            }
+        }
+        System.out.println("Stopped Program.");
+    }
+}
